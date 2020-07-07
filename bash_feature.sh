12c176e758fbe5b3c328cf8fe546176414c8566d
Baby-Ninja <akungojek17@gmail.com>
2020-07-07 17:45:15 +0900

    commit to update bash features

---
+++ bash_feature.sh
@@ -0,0 +1,8 @@
+echo ~+ to show current directory same as echo $PWD
+echo ~- to show previous directory
+echo $HOME to show home directory 
+touch files-{1..100}.txt to create 100 files
+rm files-{1..100}.txt to remove 100 files 
+echo {A..Z} to show A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+touch folders/{andy,hendra}.txt to create two fils andy.txt and hendra.txt
+touch folders/{1..10}{andy,hendra}.txt to create files 1andy.txt, 1hedra.txt, etc
